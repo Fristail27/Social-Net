0aeaef56ccecf4ba23c6b90ce76cf616cc62c561
Fristail27 <fristail27@gmail.com>
2020-11-10 22:40:41 +0300

    start

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img
+            src="https://image.jimcdn.com/app/cms/image/transf/dimension=160x10000:format=png/path/s4ea002c0228cd3e1/image/i7a22a76b3f922789/version/1514917365/image.png"
+            alt=""/>
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://caspian.travel/upload/tours/sulak_derbent/sul_derb_slide_1.jpg" alt=""/>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
