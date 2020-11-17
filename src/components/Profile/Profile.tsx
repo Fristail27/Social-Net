68c64a9c15cf1f294af7234495b92048db4f0b23
Fristail27 <fristail27@gmail.com>
2020-11-17 22:46:57 +0300

    Sdelal 18 urok

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import s from "./Profile.module.css"
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img src="https://caspian.travel/upload/tours/sulak_derbent/sul_derb_slide_1.jpg" alt=""/>
         </div>
