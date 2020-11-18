5e4b52a9bffe6506903c9e934b577cca99a54fcb
Fristail27 <fristail27@gmail.com>
2020-11-18 18:51:02 +0300

    Sverstal test dialogs

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,27 @@ import s from "./Dialogs.module.css"
 const Dialogs = (props:any) => {
     return (
         <div>
-            Dialogss
+            <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={s.dialog + " " + s.active}>
+                        Dimych
+                    </div>
+                    <div className={s.dialog}>
+                        Andrey
+                    </div>
+                    <div className={s.dialog}>
+                        Alex
+                    </div>
+                    <div className={s.dialog}>
+                        Jason
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hi</div>
+                    <div className={s.message}>How are you</div>
+                    <div className={s.message}>lol</div>
+                </div>
+            </div>
         </div>
     )
 }
