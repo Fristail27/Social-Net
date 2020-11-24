560276bae26f02a9b0c1de28c02c67ba775f8838
Fristail27 <fristail27@gmail.com>
2020-11-24 22:19:11 +0300

    add dialogItem and Message files

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,29 +1,7 @@
 import React from "react";
 import s from "./Dialogs.module.css"
-import {NavLink} from "react-router-dom";
-
-type DialogItemType = {
-    name: string,
-    id: number,
-}
-
-type MessageType = {
-    message: string,
-}
-
-const DialogItem = function (props: DialogItemType) {
-    return (
-        <div className={s.dialog + " " + s.active}>
-            <NavLink to={"/dialogs/" + props.id}>{props.name}</NavLink>
-        </div>
-    )
-}
-
-const Message = function (props: MessageType) {
-    return (
-        <div className={s.message}>{props.message}</div>
-    )
-}
+import DialogItem from "./DialogItem/DialogItem";
+import Message from "./Message/Message";
 
 const Dialogs = (props: any) => {
     let dialogsData = [
